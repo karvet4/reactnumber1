b9bcef68684042cd0aad69a98cfeef40a3f1961d
Тимур К <karvet4@mail.ru>
2020-06-20 17:17:57 +0300

    initial commit

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,9 @@ function Footer(props) {
             This is Footer {props.appVersion}
             {props.footerMenuItems.map(el => <FooterMenu key={el} item={el}/>)}
             <button onClick={() => props.attempt('Tilapia')}>Tilapia</button>
-            <p><input type= "text"/></p>
+            <p><button onClick={() => props.click('ed_1')}>click 1</button></p>
+            <p><button onClick={() => props.click('ed_2')}>click 2</button></p>
+            <p><button onClick={() => props.click('ed_3')}>click 3</button></p>
         </div>
     );
 }
